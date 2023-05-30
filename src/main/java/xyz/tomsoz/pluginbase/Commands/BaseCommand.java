b393c9cf94bbd70e2d7688eae169c3f9fc3f0c62
BuildTools <unconfigured@null.spigotmc.org>
2023-05-30 04:12:06 +0100

    Added redundability

--- src/main/java/xyz/tomsoz/pluginbase/Commands/BaseCommand.java
+++ src/main/java/xyz/tomsoz/pluginbase/Commands/BaseCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import xyz.tomsoz.pluginbase.Common;
 import xyz.tomsoz.pluginbase.Locale.Translator;
 import xyz.tomsoz.pluginbase.Text.Text;
@@ -26,7 +27,7 @@ public abstract class BaseCommand implements CommandExecutor {
     @Getter protected boolean playerOnly;
     @Getter protected boolean consoleOnly;
 
-    public BaseCommand(JavaPlugin instance, String permission, String name, String[] aliases, Argument[] args, boolean playerOnly, boolean consoleOnly) {
+    public BaseCommand(@NotNull JavaPlugin instance, @Nullable String permission, @NotNull String name, @Nullable String[] aliases, @Nullable Argument[] args, boolean playerOnly, boolean consoleOnly) {
         this.permission = permission;
         this.name = name;
         this.aliases = aliases;
@@ -59,37 +60,39 @@ public abstract class BaseCommand implements CommandExecutor {
             return true;
         }
 
-        int required = 0;
+        if (args == null) {
+            int required = 0;
 
-        StringBuilder usageStr = new StringBuilder();
-        usageStr.append(s);
+            StringBuilder usageStr = new StringBuilder();
+            usageStr.append(s);
 
-        boolean valid = true;
-        int i = 0;
-        for (Argument a : this.args) {
-            if (a.isRequired()) {
-                required++;
-                usageStr.append(" <").append(a.getName()).append(">");
-            } else {
-                usageStr.append(" [").append(a.getName()).append("]");
+            boolean valid = true;
+            int i = 0;
+            for (Argument a : this.args) {
+                if (a.isRequired()) {
+                    required++;
+                    usageStr.append(" <").append(a.getName()).append(">");
+                } else {
+                    usageStr.append(" [").append(a.getName()).append("]");
+                }
+
+                switch (a.getType()) {
+                    case PLAYER:
+                        valid = Bukkit.getPlayerExact(strings[i]) != null;
+                    case STRING:
+                        valid = true;
+                    case BOOLEAN:
+                        valid = strings[i].equalsIgnoreCase("true") || strings[i].equalsIgnoreCase("false");
+                    case INTEGER:
+                        valid = Common.checkInt(strings[i]) != null;
+                }
+                i++;
             }
 
-            switch (a.getType()) {
-                case PLAYER:
-                    valid = Bukkit.getPlayerExact(strings[i]) != null;
-                case STRING:
-                    valid = true;
-                case BOOLEAN:
-                    valid = strings[i].equalsIgnoreCase("true") || strings[i].equalsIgnoreCase("false");
-                case INTEGER:
-                    valid = Common.checkInt(strings[i]) != null;
+            if (!(args.length >= required) || !valid) {
+                Text.tell(commandSender, messages.get("commands.invalid-usage", usageStr.toString()));
+                return true;
             }
-            i++;
-        }
-
-        if (!(args.length >= required) || !valid) {
-            Text.tell(commandSender, messages.get("commands.invalid-usage", usageStr.toString()));
-            return true;
         }
 
         run(commandSender, command, s, strings);
