662dfbfc8900fbf2da88cb8192f3de2140e8fc18
Tomsoz <razertomsoz@gmail.com>
2023-06-06 21:22:58 +0100

    Added automatic command registering
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/xyz/tomsoz/pluginbase/Commands/BaseCommand.java
+++ src/main/java/xyz/tomsoz/pluginbase/Commands/BaseCommand.java
@@ -3,7 +3,7 @@ package xyz.tomsoz.pluginbase.Commands;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,32 +13,108 @@ import xyz.tomsoz.pluginbase.Common;
 import xyz.tomsoz.pluginbase.Locale.Translator;
 import xyz.tomsoz.pluginbase.Text.Text;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
  * Extend this on your command classes.
  */
-public abstract class BaseCommand implements CommandExecutor {
-    @Getter protected String permission;
-    @Getter protected String name;
-    @Getter protected String[] aliases;
-    @Getter protected Argument[] args;
-    @Getter protected JavaPlugin i;
-    @Getter protected boolean playerOnly;
-    @Getter protected boolean consoleOnly;
+public abstract class BaseCommand extends Command {
+    @Getter
+    protected String permission;
+    @Getter
+    protected String name;
+    @Getter
+    protected String description;
+    @Getter
+    protected String[] aliases;
+    @Getter
+    protected Argument[] args;
+    @Getter
+    protected JavaPlugin instance;
+    @Getter
+    protected boolean playerOnly;
+    @Getter
+    protected boolean consoleOnly;
 
-    public BaseCommand(@NotNull JavaPlugin instance, @Nullable String permission, @NotNull String name, @Nullable String[] aliases, @Nullable Argument[] args, boolean playerOnly, boolean consoleOnly) {
+    public BaseCommand(@NotNull JavaPlugin instance, @NotNull String description, @Nullable String permission, @NotNull String name, @Nullable String[] aliases, @Nullable Argument[] args, boolean playerOnly) {
+        super(name, description, "", Arrays.asList(aliases));
         this.permission = permission;
+        this.description = description;
         this.name = name;
         this.aliases = aliases;
         this.args = args;
-        this.i = instance;
+        this.instance = instance;
         this.playerOnly = playerOnly;
-        this.consoleOnly = consoleOnly;
+    }
+
+    public BaseCommand(@NotNull String description, @Nullable String permission, @NotNull String name, @Nullable String[] aliases, @Nullable Argument[] args, boolean playerOnly) {
+        super(name, description, "", Arrays.asList(aliases));
+        this.permission = permission;
+        this.description = description;
+        this.name = name;
+        this.aliases = aliases;
+        this.args = args;
+        this.playerOnly = playerOnly;
+    }
+
+    public BaseCommand(@NotNull String description, @NotNull String name, @Nullable String[] aliases, @Nullable Argument[] args, boolean playerOnly) {
+        super(name, description, "", Arrays.asList(aliases));
+        this.name = name;
+        this.description = description;
+        this.aliases = aliases;
+        this.args = args;
+        this.playerOnly = playerOnly;
+    }
+
+    public BaseCommand(@NotNull String description, @NotNull String name, @Nullable String[] aliases, boolean playerOnly) {
+        super(name, description, "", Arrays.asList(aliases));
+        this.name = name;
+        this.description = description;
+        this.aliases = aliases;
+        this.playerOnly = playerOnly;
+    }
+
+    public BaseCommand(@NotNull String description, @NotNull String name, boolean playerOnly) {
+        super(name, description, "", new ArrayList<>());
+        this.name = name;
+        this.description = description;
+        this.playerOnly = playerOnly;
+    }
+
+    public BaseCommand(@NotNull String description, @NotNull String name, @Nullable String permission, boolean playerOnly) {
+        super(name, description, "", new ArrayList<>());
+        this.name = name;
+        this.description = description;
+        this.playerOnly = playerOnly;
+        this.permission = permission;
+    }
+
+    public BaseCommand(@NotNull String description, @NotNull String name) {
+        super(name, description, "", new ArrayList<>());
+        this.name = name;
+        this.description = description;
+        this.playerOnly = false;
+    }
+
+    protected abstract void run(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args);
+
+    public void register() {
+        try {
+            final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+
+            bukkitCommandMap.setAccessible(true);
+            CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+
+            commandMap.register(this.name, this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+    public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] strings) {
         Translator messages = Translator.create();
 
         if (!s.equalsIgnoreCase(this.name) && !Arrays.asList(this.aliases).contains(this.name)) {
@@ -95,9 +171,7 @@ public abstract class BaseCommand implements CommandExecutor {
             }
         }
 
-        run(commandSender, command, s, strings);
+        run(commandSender, s, strings);
         return true;
     }
-
-    protected abstract void run(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args);
 }
