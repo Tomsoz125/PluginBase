70d9d6acc8ce83dba7be7dcb1029b7ef98d05e7e
BuildTools <unconfigured@null.spigotmc.org>
2023-06-25 23:20:47 +0100

    ok events should work now

--- src/main/java/xyz/tomsoz/pluginbase/Events/BaseEvent.java
+++ src/main/java/xyz/tomsoz/pluginbase/Events/BaseEvent.java
@@ -2,22 +2,33 @@ package xyz.tomsoz.pluginbase.Events;
 
 import lombok.Getter;
 import org.bukkit.event.Event;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import org.bukkit.event.EventPriority;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.tomsoz.pluginbase.PluginManager;
 
-public abstract class BaseEvent<E extends Event> implements Listener {
+public abstract class BaseEvent<E extends Event> {
     @Getter
     protected JavaPlugin instance;
+    @Getter
+    EventPriority priority;
+    @Getter
+    Class<E> clazz;
 
-    public BaseEvent(JavaPlugin instance) {
+    public BaseEvent(JavaPlugin instance, Class<E> clazz) {
         this.instance = instance;
+        this.priority = EventPriority.NORMAL;
+        register();
     }
 
-    @EventHandler
-    public void onEvent(E e) {
-        event(e);
+    public BaseEvent(JavaPlugin instance, Class<E> clazz, EventPriority priority) {
+        this.instance = instance;
+        this.priority = priority;
+        register();
     }
 
     protected abstract void event(E e);
+
+    public void register() {
+        PluginManager.getEventManager().register(this, clazz, priority, this::event);
+    }
 }
