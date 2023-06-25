29bbafa07cbbbc2522b180d76cffbc4053700ee5
BuildTools <unconfigured@null.spigotmc.org>
2023-06-25 22:23:03 +0100

    added event registering gg

--- src/main/java/xyz/tomsoz/pluginbase/Events/EventManager.java
+++ src/main/java/xyz/tomsoz/pluginbase/Events/EventManager.java
@@ -4,25 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import xyz.tomsoz.pluginbase.PluginManager;
 
-import java.util.ArrayList;
-
 public class EventManager {
-    public static ArrayList<Class<?>> events = new ArrayList<>();
-
-    public void registerAll() {
-        for (Class<?> c : events) {
-            Object obj = null;
-            try {
-                obj = c.newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-
-            if (obj == null) continue;
-
-            if (c.isAssignableFrom(Listener.class)) {
-                Bukkit.getPluginManager().registerEvents((Listener) obj, PluginManager.getPlugin());
-            }
-        }
+    public void register(Listener listener) {
+        Bukkit.getPluginManager().registerEvents(listener, PluginManager.getPlugin());
     }
 }
