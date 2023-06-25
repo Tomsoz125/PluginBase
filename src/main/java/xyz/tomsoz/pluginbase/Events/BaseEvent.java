8019e4dd1cfc8dcba9965b1620ae7d8be111470e
BuildTools <unconfigured@null.spigotmc.org>
2023-06-25 22:57:39 +0100

    dsihesafiubns

--- src/main/java/xyz/tomsoz/pluginbase/Events/BaseEvent.java
+++ src/main/java/xyz/tomsoz/pluginbase/Events/BaseEvent.java
@@ -6,7 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public abstract class BaseEvent<T extends Event> implements Listener {
+public abstract class BaseEvent<E extends Event> implements Listener {
     @Getter
     protected JavaPlugin instance;
 
@@ -15,9 +15,9 @@ public abstract class BaseEvent<T extends Event> implements Listener {
     }
 
     @EventHandler
-    public void onEvent(T e) {
+    public void onEvent(E e) {
         event(e);
     }
 
-    protected abstract void event(T e);
+    protected abstract void event(E e);
 }
