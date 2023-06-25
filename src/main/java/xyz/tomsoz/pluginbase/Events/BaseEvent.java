4e263227966dbc69a2f03e01d427889dde35e28b
BuildTools <unconfigured@null.spigotmc.org>
2023-06-25 23:25:40 +0100

    ok events should work now but like actually

--- src/main/java/xyz/tomsoz/pluginbase/Events/BaseEvent.java
+++ src/main/java/xyz/tomsoz/pluginbase/Events/BaseEvent.java
@@ -17,12 +17,14 @@ public abstract class BaseEvent<E extends Event> {
     public BaseEvent(JavaPlugin instance, Class<E> clazz) {
         this.instance = instance;
         this.priority = EventPriority.NORMAL;
+        this.clazz = clazz;
         register();
     }
 
     public BaseEvent(JavaPlugin instance, Class<E> clazz, EventPriority priority) {
         this.instance = instance;
         this.priority = priority;
+        this.clazz = clazz;
         register();
     }
 
